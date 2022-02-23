8762626ea006df21bf918eee2b83f0ee931cfff3
Frankiz <30655500+Noduns@users.noreply.github.com>
2022-02-23 19:01:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ def cell_options(grid,i,j, verbose = 0):
 
     if grid[i,j]!=0: # there is a digits at the location
         c_options = np.zeros(9)
-
     else:
         for k in range(9):
             row_options[k] -= np.any(grid[i,:]==k+1)
@@ -26,27 +25,14 @@ def cell_options(grid,i,j, verbose = 0):
 
             for p,q in product(range(3),range(3)):
                 if grid[p+i_box*3,q+j_box*3]==k+1: box_options[k] -= 1
-
-            c_options = np.multiply(np.multiply(col_options,row_options),box_options)
-
-        if verbose >=3:
-            print("--- OPTIONS or possible digits --")
-            print(i,j)
-            print(f'row_options  {row_options}')
-            print(f'col_options  {col_options}')
-            print(f'box_options  {box_options}')
-            print(f'cell options {c_options}')
-            input("---")
-
-    return c_options
-
+            cell_options = np.multiply(np.multiply(col_options,row_options),box_options)
+    return cell_options
 
 def update_grid_options(grid,verbose=0,options = np.zeros((9,9,9))):
     """Computes the available options for all grid cells"""
-    for i,j in product(range(9), range(9)):
+    for i,j in product(range(9), range(9)):  
         options[i,j,:] = cell_options(grid,i,j,verbose)
     return options
-
 
 def input_cell(grid,i,j,digit,nb_empty,verbose=0):
     """update the grid with a digit, the number of empties, and the grid options """
@@ -56,16 +42,13 @@ def input_cell(grid,i,j,digit,nb_empty,verbose=0):
     if verbose>=1: print( f' Entered {digit} at {i},{j}')
     return grid,nb_empty,options
 
-
 def direct_solver(grid, verbose = 0):
     """ The function tries to solves the sudoko without doing any hypothesis
-    args:
-        grid
 
     returns:
-        has_conclusion : boolean 
-        is_solvable : boolean
-        grid : the completed grid
+        is_solvable : boolean = True if grid is solved, False if grid is unsolvable
+        has_conclusion : boolean False if function was able to determine if grid is solvable   
+        grid : the updated grid
     """
     options = update_grid_options(grid, verbose)
     
@@ -147,11 +130,8 @@ def hypothesis_solver(grid, verbose = 0):
             if sum(options[i,j,:])!=0 and not np.all(potential_hyp[i,j,:]==0):
                 m = np.where(potential_hyp[i,j,:]==1)[0][0]
                 potential_hyp[i,j,m]=0
-                grid,nb_empty,options = input_cell(grid,i,j,m+1,nb_empty,verbose=0)
-                
-                if verbose >=2 :
-                    print(f"{i},{j} : options are {1+np.where(mem_options[i,j,:]==1)[0]}")
-                    input(f"      trying a {m+1}")
+                grid,nb_empty,options = input_cell(grid,i,j,m+1,nb_empty)
+                if verbose >=2 : input(f"{i},{j} :       trying a {m+1}")
                 break
 
         has_conclusion, is_solvable, temp_grid = direct_solver(grid)
